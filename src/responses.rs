d70c1a5ec6a762051c201c5e94ebe031a634b3fa
Pacman <pacman@apollo.farm>
2022-10-28 16:18:35 +0200

    feat: add merge_responses

---
+++ src/responses.rs
@@ -0,0 +1,15 @@
+use cosmwasm_std::Response;
+
+/// Merge several Response objects into one. Currently ignores the data fields.
+///
+/// Returns a new Response object.
+pub fn merge_responses(responses: Vec<Response>) -> Response {
+    let mut merged = Response::default();
+    for response in responses {
+        merged = merged
+            .add_attributes(response.attributes)
+            .add_events(response.events)
+            .add_submessages(response.messages);
+    }
+    merged
+}
