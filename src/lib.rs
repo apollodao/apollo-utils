7bc02f3c231666880540490facea4bc516584162
Bobby <bobby@apollo.farm>
2022-11-09 14:24:58 +0100

    Fix response_prefix! macro errors using with_dollar_sign! macro
    Signed-off-by: Bobby <bobby@apollo.farm>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-//#![cfg_attr(not(doc), no_main)]
-#![feature(macro_metavar_expr)]
 pub mod assets;
 pub mod responses;
 pub mod submessages;
--- src/responses.rs
+++ src/responses.rs
@@ -14,66 +14,79 @@ pub fn merge_responses(responses: Vec<Response>) -> Response {
     merged
 }
 
+// Utility macro for response_prefix! macro below
+#[macro_export]
+macro_rules! with_dollar_sign {
+    ( $( $body:tt )* ) => {
+        macro_rules! __with_dollar_sign { $($body)* }
+        __with_dollar_sign!($);
+    }
+}
+
 /// Super-macro for creating the prefix in Events that might differ from contract to contract
 /// Simply call `response_prefix!("foo/bar")`, which expands to a definition `response!` with
 /// "foo/bar" as the prefix. The subsequent Events will be named "foo/bar/<event name>".
 #[macro_export]
 macro_rules! response_prefix {
     ( $prefix:literal ) => {
-        /// Macro for generating Response objects with Events, Attributes and Messages
-        ///
-        /// Variants
-        ///  - response!() - returns a new, empty Response
-        ///  - response!("event") - returns a new Response with Event named "event"
-        ///  - response!(resp, "event") - attaches Event "event" to Response `resp`
-        ///  - response!("event", [(k, v), (j, u), ...]) - returns Response with
-        ///    Event "event" and Attributes from series (not array) of tuples
-        ///  - response!(resp, "event", [(k, v), (j, u), ...]) - same as above but
-        ///    attaching to existing Response `resp`
-        ///  - response!("event", [(k, v), (j, u), ...], [m, n, ...]) - returns Response
-        ///    with Event "event", Attributes `(k, v)` etc., and Messages `m`, `n`, etc.
-        ///  - response!(resp, "event", [(k, v), (j, u), ...], [m, n, ...]) - same as
-        ///    above but attached to Response `resp`
-        macro_rules! response {
-            () => {
-                cosmwasm_std::Response::<cosmwasm_std::Empty>::new()
-            };
-            ( $$response:expr, $$event_name:literal, [ $$( ($$key:literal, $$value:expr) ),* ] ) => {
-                {
-                    // needed because things get weird with macros
-                    #[allow(unused_mut)]
-                    let mut attrs: Vec<cosmwasm_std::Attribute> = Vec::new();
-                    $$(
-                        attrs.push(cosmwasm_std::attr($$key, $$value));
-                    )*
-                    let event = cosmwasm_std::Event::new(
-                            format!("{}/{}",
-                            String::from($prefix),
-                            String::from($$event_name)))
-                        .add_attributes(attrs.clone());
-                    $$response.add_attributes(attrs).add_event(event)
+        with_dollar_sign! {
+            ( $d:tt ) => {
+                /// Macro for generating Response objects with Events, Attributes and Messages
+                ///
+                /// Variants
+                ///  - response!() - returns a new, empty Response
+                ///  - response!("event") - returns a new Response with Event named "event"
+                ///  - response!(resp, "event") - attaches Event "event" to Response `resp`
+                ///  - response!("event", [(k, v), (j, u), ...]) - returns Response with
+                ///    Event "event" and Attributes from series (not array) of tuples
+                ///  - response!(resp, "event", [(k, v), (j, u), ...]) - same as above but
+                ///    attaching to existing Response `resp`
+                ///  - response!("event", [(k, v), (j, u), ...], [m, n, ...]) - returns Response
+                ///    with Event "event", Attributes `(k, v)` etc., and Messages `m`, `n`, etc.
+                ///  - response!(resp, "event", [(k, v), (j, u), ...], [m, n, ...]) - same as
+                ///    above but attached to Response `resp`
+                macro_rules! response {
+                    () => {
+                        cosmwasm_std::Response::<cosmwasm_std::Empty>::new()
+                    };
+                    ( $d response:expr, $d event_name:literal, [ $d( ($d key:literal, $d value:expr) ),* ] ) => {
+                        {
+                            // needed because things get weird with macros
+                            #[allow(unused_mut)]
+                            let mut attrs: Vec<cosmwasm_std::Attribute> = Vec::new();
+                            $d(
+                                attrs.push(cosmwasm_std::attr($d key, $d value));
+                            )*
+                            let event = cosmwasm_std::Event::new(
+                                    format!("{}/{}",
+                                    String::from($prefix),
+                                    String::from($d event_name)))
+                                .add_attributes(attrs.clone());
+                            $d response.add_attributes(attrs).add_event(event)
+                        }
+                    };
+                    ( $d response:expr, $d event_name:literal ) => {
+                        response!($d response, $d event_name, [])
+                    };
+                    ( $d event_name:literal ) => {
+                        response!(response!(), $d event_name)
+                    };
+                    ( $d event_name:literal, [ $d( ($d key:literal, $d value:expr) ),* ] ) => {
+                        response!(response!(), $d event_name, [ $d(($d key, $d value)),* ])
+                    };
+                    ( $d response:expr, $d event_name:literal, [ $d( ($d key:literal, $d value:expr) ),* ], [ $d( $d msg:expr ),* ] ) => {
+                        {
+                            let mut msgs = Vec::new();
+                            $d(
+                                msgs.push($d msg);
+                            )*
+                            response!($d response, $d event_name, [ $d(($d key, $d value)),* ]).add_messages(msgs)
+                        }
+                    };
+                    ( $d event_name:literal, [ $d( ($d key:literal, $d value:expr) ),* ], [ $d( $d msg:expr ),* ] ) => {
+                        response!(response!(), $d event_name, [ $d(($d key, $d value)),* ], [$d($d msg),*])
+                    };
                 }
-            };
-            ( $$response:expr, $$event_name:literal ) => {
-                response!($$response, $$event_name, [])
-            };
-            ( $$event_name:literal ) => {
-                response!(response!(), $$event_name)
-            };
-            ( $$event_name:literal, [ $$( ($$key:literal, $$value:expr) ),* ] ) => {
-                response!(response!(), $$event_name, [ $$(($$key, $$value)),* ])
-            };
-            ( $$response:expr, $$event_name:literal, [ $$( ($$key:literal, $$value:expr) ),* ], [ $$( $$msg:expr ),* ] ) => {
-                {
-                    let mut msgs = Vec::new();
-                    $$(
-                        msgs.push($$msg);
-                    )*
-                    response!($$response, $$event_name, [ $$(($$key, $$value)),* ]).add_messages(msgs)
-                }
-            };
-            ( $$event_name:literal, [ $$( ($$key:literal, $$value:expr) ),* ], [ $$( $$msg:expr ),* ] ) => {
-                response!(response!(), $$event_name, [ $$(($$key, $$value)),* ], [$$($$msg),*])
             };
         }
     };
