339da35241ace1d04670dea0f7d4fc6f017dc959
Sturdy <sturdy@apollo.farm>
2022-11-21 12:35:28 +0100

    feat: Add IntoElementwise trait

--- src/iterators.rs
+++ src/iterators.rs
@@ -15,3 +15,19 @@ where
             .collect::<Result<Vec<_>, E>>()
     }
 }
+
+pub trait IntoElementwise<A, B: Into<A>>: IntoIterator {
+    /// Performs into on each element of the iterator and collects the
+    /// results into a Vec.
+    fn into_elementwise(self) -> Vec<A>;
+}
+
+impl<A, B, I> IntoElementwise<A, B> for I
+where
+    B: Into<A>,
+    I: IntoIterator<Item = B>,
+{
+    fn into_elementwise(self) -> Vec<A> {
+        self.into_iter().map(|x| x.into()).collect()
+    }
+}
